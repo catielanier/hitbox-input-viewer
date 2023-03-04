6bad31c15b53c196b6d52ea0ecd7f29ada0deccd
Corey Lanier <iam@coreylanier.com>
2023-03-04 15:14:18 -0500

    detect connect and disconnect of controller

---
+++ src/App.svelte
@@ -0,0 +1,53 @@
+<script>
+	let haveEvents = 'ongamepadconnected' in window;
+	let gamepads = {}
+	const removeGamepad = (index) => delete gamepads.index;
+	window.addEventListener("gamepaddisconnected", (e) => {
+		removeGamepad(e.gamepad.index);
+	})
+	const addGamepad = (gamepad) => {
+		gamepads = {
+			...gamepads,
+			[gamepad.index]: {
+				buttons: gamepad.buttons,
+				id: gamepad.id,
+				axes: gamepad.axes
+			}
+		}
+	}
+	window.addEventListener("gamepadconnected", (e) => {
+		addGamepad(e.gamepad)
+	})
+</script>
+
+<main>
+	{#if Object.keys(gamepads).length}
+	<div class="hitbox-grid">
+
+	</div>
+	{:else}
+		<p>Please connect a gamepad.</p>
+	{/if}
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
