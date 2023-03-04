2055facbdfe001ce997f429f3000fe37c6867da0
Corey Lanier <iam@coreylanier.com>
2023-03-04 15:34:51 -0500

    switch to vite

--- src/App.svelte
+++ src/App.svelte
@@ -1,54 +1,46 @@
 <script>
-	let haveEvents = 'ongamepadconnected' in window;
-	let gamepads = {}
-	const removeGamepad = (index) => delete gamepads.index;
-	window.addEventListener("gamepaddisconnected", (e) => {
-		removeGamepad(e.gamepad.index);
-	})
-	const addGamepad = (gamepad) => {
-		gamepads = {
-			...gamepads,
-			[gamepad.index]: {
-				buttons: gamepad.buttons,
-				id: gamepad.id,
-				axes: gamepad.axes
-			}
-		}
-	}
-	window.addEventListener("gamepadconnected", (e) => {
-		addGamepad(e.gamepad)
-	})
+  import svelteLogo from './assets/svelte.svg'
+  import Counter from './lib/Counter.svelte'
 </script>
 
 <main>
-	{#if Object.keys(gamepads).length}
-		<div class="hitbox-grid">
-			
-		</div>
-	{:else}
-		<p>Please connect a gamepad.</p>
-	{/if}
+  <div>
+    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
+      <img src="/vite.svg" class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
+  </div>
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	.hitbox-grid {
-		width: 100%;
-		display: grid;
-		grid-template-rows: repeat(105, 1fr);
-		grid-template-columns: repeat(172, 1fr);
-	}
-
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
 </style>
