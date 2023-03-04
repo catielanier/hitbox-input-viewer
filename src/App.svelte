59061c0a47bb9a18d1c4879f8b226b738f8cec36
Corey Lanier <iam@coreylanier.com>
2023-03-04 16:11:42 -0500

    create button containers

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,86 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+	import EmptyDivs from "./EmptyDivs.svelte";
+	$: haveEvents = 'ongamepadconnected' in window;
+	$: gamepads = {}
+	const removeGamepad = (index) => delete gamepads.index;
+	window.addEventListener("gamepaddisconnected", (e) => {
+		removeGamepad(e.gamepad.index);
+	})
+	const addGamepad = (gamepad) => {
+		gamepads = {
+			...gamepads,
+			[gamepad.index]: {
+				buttons: gamepad.buttons,
+				id: gamepad.id,
+				axes: gamepad.axes
+			}
+		}
+	}
+	const scanGamepads = () => {
+		var detectedGamepads = navigator.getGamepads
+      ? navigator.getGamepads()
+      : navigator.webkitGetGamepads
+      ? navigator.webkitGetGamepads()
+      : []
+
+    // Loop through all detected controllers and add if not already in state
+    for (var i = 0; i < detectedGamepads.length; i++) {
+      if (detectedGamepads[i]) {
+        addGamepad(detectedGamepads[i])
+      }
+    }
+		console.log(gamepads)
+	}
+
+	$: if (!haveEvents) {
+		setInterval(scanGamepads, 10000)
+	}
+	window.addEventListener("gamepadconnected", (e) => {
+		addGamepad(e.gamepad)
+	})
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+	{#if Object.keys(gamepads).length}
+		<div class="hitbox-grid">
+			<div id="hitbox-up"><div class="button-inner button-active-{gamepads[0].buttons[12].touched}" /></div>
+			<div id="hitbox-down"><div class="button-inner button-active-{gamepads[0].buttons[13].touched}" /></div>
+			<div id="hitbox-left"><div class="button-inner button-active-{gamepads[0].buttons[14].touched}" /></div>
+			<div id="hitbox-right"><div class="button-inner button-active-{gamepads[0].buttons[15].touched}" /> </div>
+			<div id="hitbox-jab"><div class="button-inner button-active-{gamepads[0].buttons[2].touched}" /></div>
+			<div id="hitbox-strong"><div class="button-inner button-active-{gamepads[0].buttons[3].touched}" /></div>
+			<div id="hitbox-fierce"><div class="button-inner button-active-{gamepads[0].buttons[5].touched}" /></div>
+			<div id="hitbox-short"><div class="button-inner button-active-{gamepads[0].buttons[0].touched}" /></div>
+			<div id="hitbox-forward"><div class="button-inner button-active-{gamepads[0].buttons[1].touched}" /></div>
+			<div id="hitbox-roundhouse"><div class="button-inner button-active-{gamepads[0].buttons[7].touched}" /></div>
+			<div id="hitbox-3p"><div class="button-inner button-active-false" /></div>
+			<div id="hitbox-3k"><div class="button-inner button-active-false" /></div>
+			<EmptyDivs />
+		</div>
+	{:else}
+		<p>Please connect a gamepad.</p>
+	{/if}
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	.hitbox-grid {
+		width: 100%;
+		display: grid;
+		grid-template-rows: repeat(105, 1fr);
+		grid-template-columns: repeat(172, 1fr);
+	}
+
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
 </style>
