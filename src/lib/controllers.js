896b87a1c21270955363e60bfbe815e68a2e4ee7
Corey Lanier <iam@coreylanier.com>
2023-03-08 18:01:22 -0500

    add devices to controller array for hotplugging

---
+++ src/lib/controllers.js
@@ -0,0 +1,21 @@
+const controllers = [
+  {
+    id: "Brook Controller (STANDARD GAMEPAD Vendor: 045e Product: 028e)",
+    buttons: {
+      jab: 2,
+      strong: 3,
+      fierce: 5,
+      short: 0,
+      forward: 1,
+      roundhouse: 7,
+      ppp: 4,
+      kkk: 6,
+      up: 12,
+      down: 13,
+      left: 14,
+      right: 15,
+    },
+  },
+];
+
+export default controllers;
